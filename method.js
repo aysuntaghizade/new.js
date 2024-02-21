3e6911474fc79dd5dfec10b913f460d138549482
Aysun Taghizade <aysuntaghizade@gmail.com>
2024-02-21 16:14:16 +0400

    First commi

---
+++ method.js
@@ -0,0 +1,27 @@
+//! Task1-Find all occurrences of "Spain"; match()
+// const text ="The rain in Spain stays mainly in the plain. In Spain, in April, the rain is a pain."
+// const regex=/[spain]/g;
+// const found=text.match(regex);
+// console.log(found);
+
+//! Task2- Find positions of 'rain';matchAll()
+// const paraghraph= "The rain in Spain stays mainly in the plain. In Spain, in April, the rain is a pain.";
+// const  found= paraghraph.matchAll('rain');
+// console.log(...found);
+
+//! Task3-Replace 'Spain' with 'France';replace()
+// const paragraph= "The rain in Spain stays mainly in the plain. In Spain, in April, the rain is a pain.";
+// const regex = /Spain/i;
+// console.log(paragraph.replace(regex, 'France'));
+
+//! Task4-Replace all 'rain' with 'sun';replaceAll()
+//  const paragraph= "The rain in Spain stays mainly in the plain. In Spain, in April, the rain is a pain.";
+//  const regex=/rain/gi;
+//  console.log((paragraph.replaceAll(regex,'sun')));
+
+//! Task5-Search for 'plain';search()
+// const paragraph= "The rain in Spain stays mainly in the plain. In Spain, in April, the rain is a pain.";
+// const searched=paragraph.match('plain')
+// console.log(searched);
+
+
